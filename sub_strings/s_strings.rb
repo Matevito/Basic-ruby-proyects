1b52683f59967f5edba45b80b46948349a05eed8
Mateo <madiazt@unal.edu.co>
2020-10-08 16:31:27 -0500

    corrected hash value bugg

--- sub_strings/s_strings.rb
+++ sub_strings/s_strings.rb
@@ -1,24 +1,32 @@
+#s_strings.rb found substrings in a text with words provided in a dictionary; the output is a hash with the number of appereances
 
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 
 def substrings(text, dictionary)
-    text = text.downcase
+    text = text.downcase.split
 
-    #takes the words in dictionary that are substring of text
-    substrings_words = dictionary.select {|word| text.include?(word)}
+    substrings_words = []
 
-    puts substrings_words
-
-    substring = Hash.new(0)
-    #make the hash
-    substrings_words.each do |word|
-        substring[word] += 1
+    #iterate all words in the text
+    text.each do |word_text|
+        #takes the words in dictionary that are substring of word_text
+        substrings_words += dictionary.select {|word| word_text.include?(word)}
     end
 
-    substring
-    
+    #make the hash
+    substring_hash = Hash.new(0)
+    substrings_words.each do |word|
+        substring_hash[word] += 1
+    end
+
+    substring_hash
 
 end
 
 #test
-puts substrings("Howdy partner, sit down! How's it going?", dictionary)
+
+substrings("Howdy partner, sit down! How's it going?", dictionary) 
+#=>{"how"=>2, "howdy"=>1, "part"=>1, "partner"=>1, "it"=>2, "i"=>3, "sit"=>1, "down"=>1, "own"=>1, "go"=>1, "going"=>1}
+
+substrings("below", dictionary)
+#=>{"below"=>1, "low"=>1}
