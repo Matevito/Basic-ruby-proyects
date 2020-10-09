f454d7cf2fb59c28ba5d7a4448f7626d68810537
Mateo <madiazt@unal.edu.co>
2020-10-08 20:31:29 -0500

    added todo's for improvement

--- bubble_sort/bubble_sort.rb
+++ bubble_sort/bubble_sort.rb
@@ -2,6 +2,8 @@ def bubble_sort (num_list)
     loop do
         is_sorted = true
         new_list = [num_list[0]]
+
+        #todo: this can be simplified in a function (think a diferent way to check if the list is sorted)
         for i in (0..num_list.length-1)
             current_num = new_list.last
             right_num = num_list[i+1]
@@ -18,10 +20,11 @@ def bubble_sort (num_list)
 
             end
         end
+        #actualize list 
         num_list = new_list
         break if is_sorted == true 
     end
     num_list
 end
 
-print bubble_sort([4,3,78,2,0,2])
+print bubble_sort([4,3,78,2,0,2]) #output=> [0, 2, 2, 3, 4, 78
