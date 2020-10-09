38926d5a9060fef3a72081b9d33c23c314210d30
Mateo <madiazt@unal.edu.co>
2020-10-08 20:27:48 -0500

    functional code

--- bubble_sort/bubble_sort.rb
+++ bubble_sort/bubble_sort.rb
@@ -1,26 +1,27 @@
 def bubble_sort (num_list)
-    is_sorted = true
     loop do
+        is_sorted = true
         new_list = [num_list[0]]
         for i in (0..num_list.length-1)
             current_num = new_list.last
             right_num = num_list[i+1]
-            
+
             #break when index = last_index
             break if i == num_list.length-1
-                
 
             if right_num < current_num
                 new_list.pop
                 new_list.push(right_num,current_num)
+                is_sorted = "false"
             else
                 new_list.push(right_num)
 
             end
         end
-        print new_list
-        break
+        num_list = new_list
+        break if is_sorted == true 
     end
+    num_list
 end
 
-bubble_sort([4,3,78,2,0,2])
+print bubble_sort([4,3,78,2,0,2])
