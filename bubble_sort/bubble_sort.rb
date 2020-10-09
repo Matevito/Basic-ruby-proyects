d162c8311d3900b24bf8ee9759e01816e6f15f83
Mateo <madiazt@unal.edu.co>
2020-10-08 20:40:40 -0500

    simplified with a function for one sort

--- bubble_sort/bubble_sort.rb
+++ bubble_sort/bubble_sort.rb
@@ -1,31 +1,35 @@
 def bubble_sort (num_list)
     loop do
-        is_sorted = true
-        new_list = [num_list[0]]
-
-        #todo: this can be simplified in a function (think a diferent way to check if the list is sorted)
-        for i in (0..num_list.length-1)
-            current_num = new_list.last
-            right_num = num_list[i+1]
-
-            #break when index = last_index
-            break if i == num_list.length-1
-
-            if right_num < current_num
-                new_list.pop
-                new_list.push(right_num,current_num)
-                is_sorted = "false"
-            else
-                new_list.push(right_num)
-
-            end
-        end
+        new_list = one_sort(num_list)
+        
+        break if new_list == num_list
 
         #actualize list 
         num_list = new_list
-        break if is_sorted == true 
     end
     return num_list
+end
+
+def one_sort (num_list)
+    #input: num_list; output: the result of one check with bubble sort method
+    new_list = [num_list[0]]
+
+    for i in (0..num_list.length-1)
+        current_num = new_list.last
+        right_num = num_list[i+1]
+
+        #return when index = last_index; cause this means the list is complete
+        return new_list if i == num_list.length-1
+
+        if right_num < current_num
+            new_list.pop
+            new_list.push(right_num,current_num)
+                
+        else
+            new_list.push(right_num)
+
+        end
+    end
 end
 
 print bubble_sort([4,3,78,2,0,2]) #output=> [0, 2, 2, 3, 4, 78]
