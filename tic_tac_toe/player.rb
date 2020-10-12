725a021b3c6f5e6800882abb0f21ef93967eba22
Mateo <madiazt@unal.edu.co>
2020-10-12 17:46:30 -0500

    added automatic rubocop corrections

--- tic_tac_toe/player.rb
+++ tic_tac_toe/player.rb
@@ -1,24 +1,27 @@
+# frozen_string_literal: true
+
 # player.rb
-require_relative "game_board"
+require_relative 'game_board'
 
 class Player < Game_ttt
-    attr_reader :name, :board_sign, :turn, :board
-    def initialize(name, board_sign)
-        @name = name
-        @board_sign = board_sign
+  attr_reader :name, :board_sign, :turn, :board
+  def initialize(name, board_sign)
+    @name = name
+    @board_sign = board_sign
+  end
+
+  def make_move
+    puts "player #{name}, make your move: "
+    move = gets.chomp
+    # esception if the choosen cell has been already selected
+    until @@board[move] == ' '
+      puts 'the cell has already been taken. Choose another cell'
+      puts "player #{name}, make your move: "
+      move = gets.chomp
     end
-    def make_move
-        puts "player #{name}, make your move: "
-        move = gets.chomp
-        # todo: what if the choosen move has been already selected?
-        until @@board[move] == " "
-            puts "the cell has already been taken. Choose another cell"
-            puts "player #{name}, make your move: "
-            move = gets.chomp
-        end
-        # perform the move in the board
-        @@board[move] = board_sign
-        Game_ttt.print_gui
-        @@turn += 1
-    end
+    # perform the move in the board
+    @@board[move] = board_sign
+    Game_ttt.print_gui
+    @@turn += 1
+  end
 end
