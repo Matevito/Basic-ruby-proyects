4eafe7221fc92c4e4f8632a6d1f945efdcc96f32
Mateo <madiazt@unal.edu.co>
2020-10-13 10:15:34 -0500

    skeleton for game options

--- mastermind/main.rb
+++ mastermind/main.rb
@@ -5,32 +5,49 @@ require_relative  "computer"
 
 # game logic
 # player_guesser code
+print_rules()
+intro_input = gets.chomp
 loop do
-    #puts "Player, put your name: "
-    #name = gets.chomp
-    name = "charles"                                  # for construction reasons
-    player_1 = Human_player.new(name)
-    mastermind = Cpu.new
-    current_game = Mastermind.new
+    name = "charles" 
+    if intro_input == "1"
+    #guessing the code of a cpu
+        #puts "Player, put your name: "
+        #name = gets.chomp
+        player_1 = Human_player.new(name)
+        mastermind = Cpu.new
+        current_game = Mastermind.new
 
+        # generating secret code
+        secret_code = mastermind.random_guess
+        turn = 12
 
-    # generating secret code
-    #secret_code = mastermind.random_guess
-    secret_code = [3,1,1,5]
-    turn = 12
+        # play the game
+        for i in (1..12)
+            puts "\nguesses left: #{turn}"
+            # this is needed to solve a bugg
+            code_to_check = current_game.make_copy(secret_code)
+            guess = player_1.take_a_guess
 
-    # play the game
-    for i in (1..12)
-        puts "\nguesses left: #{turn}"
-        p secret_code
-        code_to_check = current_game.make_copy(secret_code)
-        guess = player_1.take_a_guess
-        guess_results = current_game.check_guess(guess, code_to_check)
-
-        current_game.print_game_evaluation(guess_results)
-        break if guess_results[0] == 4
-
-        turn -=1
+            # check the guess
+            guess_results = current_game.check_guess(guess, code_to_check)
+            current_game.print_game_evaluation(guess_results)
+            break if guess_results[0] == 4
+            turn -=1
+        end
+        
+        # result message
+        if guess_results[0] == 4
+            puts "\nCongratulations #{name}, the secret code was #{secret_code}"
+        else
+            puts "\nNo, the secret code was #{secret_code.join}."
+        end
+    elsif intro_input == "2"
+    # vs cpu easy mode
+        
+    elsif intro_input == "3"
+    # todo:vs cpu hard mode
     end
-    break
+    # fence post bugg
+    print_rules()
+    intro_input = gets.chomp
 end
