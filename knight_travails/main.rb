e1ae00d9ca9cb5303194c23a2e100bce6e6b00ea
Mateo <madiazt@unal.edu.co>
2020-10-22 08:44:49 -0500

    typo

--- knight_travails/main.rb
+++ knight_travails/main.rb
@@ -2,5 +2,5 @@ require_relative "objects"
 require_relative "chess_notation"
 
 knight_piece = Knight.new()
-#p knight_piece.knight_moves([0,0],[3,3]) # => [[0, 0], [1, 2], [3, 3]]
+p knight_piece.knight_moves([0,0],[3,3]) # => [[0, 0], [1, 2], [3, 3]]
 p knight_piece.knight_moves([1,2],[7,7]) # => [[1, 2], [2, 4], [3, 6], [4, 4], [5, 6], [7, 7]]
